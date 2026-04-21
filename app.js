dd6ba18cc0645f29223e833d704a4f7b347d8d93
Pedro <pedrozamoracubillo@gmail.com>
2026-04-21 14:16:38 -0600

    conexion a servidor online

--- app.js
+++ app.js
@@ -205,7 +205,7 @@ if (form && btn) {
     };
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/contacto", {
+      const res = await fetch("fetch("https://komp.onrender.com/contacto")", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
